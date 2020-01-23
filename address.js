f69bd1f797caa9af1e9aa8f5c42ece70039c1163
Dustin Murdock <dmurdock2@gmail.com>
2020-01-23 14:07:43 -0600

    initial commit

--- address.js
+++ address.js
@@ -1,19 +1,37 @@
 
-const container = document.querySelector("#addressList")
+// const container = document.querySelector("#addressList")
 
-document.querySelector("#saveEntry").addEventListener("click", event => {
+// document.querySelector("#saveEntry").addEventListener("click", event => {
    
-    const personName = document.querySelector("#fullName").value
-    const personAddress = document.querySelector("#address").value
+//     const personName = document.querySelector("#fullName").value
+//     const personAddress = document.querySelector("#address").value
 
 
-    container.innerHTML += `
+//     container.innerHTML += `
+//         <section>
+//             <h1>${personName}</h1>
+//             <div>${personAddress}</div>
+//         </section>
+//     `
+
+// })
+
+
+
+const boxOfWishes = document.querySelector("#wishList");
+
+
+
+document.querySelector("#saveItem").addEventListener("click", () => {
+    const currentWish = document.querySelector("#lovedItem").value;
+    const storeForWish = document.querySelector("#purchaseStore").value;
+
+    boxOfWishes.innerHTML += `
         <section>
-            <h1>${personName}</h1>
-            <div>${personAddress}</div>
-        </section>
-    `
-
+            <h1>I can purchase ${currentWish} at ${storeForWish}.</h1>
+        </section>`;
+    
+  
 })
 
-const boxOfWishes
+
