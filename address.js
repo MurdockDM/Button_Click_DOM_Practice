821bbb13848b91c07a4154ae33aa4dc578d3765b
Dustin Murdock <dmurdock2@gmail.com>
2020-01-23 09:41:48 -0600

    initial commit

---
+++ address.js
@@ -0,0 +1,19 @@
+
+const container = document.querySelector("#addressList")
+
+document.querySelector("#saveEntry").addEventListener("click", event => {
+   
+    const personName = document.querySelector("#fullName").value
+    const personAddress = document.querySelector("#address").value
+
+
+    container.innerHTML += `
+        <section>
+            <h1>${personName}</h1>
+            <div>${personAddress}</div>
+        </section>
+    `
+
+})
+
+const boxOfWishes
